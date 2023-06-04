6fab17600e22954c1ea2f94ce171371b1bacfecc
Yoshiki Shin'ya <785431ys@gmail.com>
2023-06-04 14:14:00 +0900

    JavaScriptでTODOアプリの作成

--- src/index.js
+++ src/index.js
@@ -1,216 +1,85 @@
-/**
-* const,let等の変数宣言
-*/
-//var val1 = "var変数";
-//console.log(val1);
-//
-//// var変数は上書き可能
-//val1 = "var変数を上書き";
-//console.log(val1);
-//
-//// var変数は再宣言可能
-//var val1 = "var変数を再宣言";
-//console.log(val1);
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-//let val2 = "let変数";
-//console.log(val2);
-//
-//// letは上書きが可能
-//val2 = "let変数を上書き";
-//console.log(val2);
-//
-//// letは再宣言不可能
-////let val2 = "let変数を再宣言";
+  createIncompleteList(inputText);
+};
 
-//const val3 = "const変数";
-//console.log(val3);
-//
-//// const変数は上書き不可
-////val3 = "const変数を上書き";
-//
-//const val3 = "const変数を再宣言";
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
 
-// constで定義したオブジェクトはプロパティの変更が可能
-//const val4 = {
-//    name: "新屋",
-//    age: 30,
-//};
-//val4.name = "shin'ya";
-//val4.address = "Okinawa";
-//console.log(val4);
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
+// div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
 
-// constで定義した配列はプロパティの変更が可能
-//const val5 = ['dog', 'cat'];
-//val5[0] = "bird";
-//val5.push("monkey");
-//console.log(val5);
+  // liタグ生成
+  const li =  document.createElement("li");
+  li.innerText = text;
 
-/**
-* テンプレート文字列
-*/
-//const name = "新屋";
-//const age = 30;
-//
-//// 従来の方法
-//const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-//console.log(message1);
-//
-//// テンプレート文字列を用いた方法
-//const message2 = `私の名前は${name}です。年齢は${age}です。`;
-//console.log(message2);
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+  // 押された削除ボタンの親タグ(div)を未完了リストから削除
+  deleteFromIncompleteList(completeButton.parentNode);
 
-/**
-* アロー関数
-*/
-// 従来の関数
-//function func1(str) {
-//    return str;
-//}
-//console.log(func1("func1です。"));
-//
-//// アロー関数
-//const func2 = (str) => str;
-//console.log(func2("func2です。"));
-//
-//const func3 = (num1, num2) => {
-//    return num1 + num2;
-//};
-//
-//console.log(func3(10, 20));
+  //完了リストに追加する要素
+  const addTarget = completeButton.parentNode;
 
-/**
-* 分割代入
-*/
-//const myProfile = {
-//    name: "新屋",
-//    age: 30,
-//};
-//
-//const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-//console.log(message1);
-//
-//const { name, age } = myProfile;
-//const message2 = `名前は${name}です。年齢は${age}歳です。`;
-//console.log(message2);
+  // TODO内容テキストを取得
+  const text = addTarget.firstElementChild.innerText;
 
-//const myProfile = ['新屋', 30];
-//const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-//console.log(message3);
-//
-//const [name, age] = myProfile;
-//const message4 = `名前は${name}です。年齢は${age}歳です。`;
-//console.log(message4);
+  // div以下を初期化
+  addTarget.textContent = null;
 
-/**
-* デフォルト値、引数など
-*/
-//const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
-//sayHello("新屋");
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = text;
 
-/**
-* スプレッド構文...
-*/
-// 配列の展開
-//const arr1 = [1, 2];
-////console.log(arr1);
-////console.log(...arr1);
-//
-//const sumFunc = (num1, num2) => console.log(num1 + num2);
-//sumFunc(arr1[0],arr1[1]);
-//sumFunc(...arr1);
+  // buttonタグ生成
+  const backButton = document.createElement("button");
+  backButton.innerText = "戻す";
+  backButton.addEventListener("click", () => {
+    // 押された戻すボタンの親タグ(div)を完了リストから削除
+    const deleteTarget = backButton.parentNode;
+    document.getElementById("complete-list").removeChild(deleteTarget);
 
-// まとめる
-//const arr2 = [1, 2, 3, 4, 5];
-//const [num1, num2, ...arr3] = arr2;
-//console.log(num1);
-//console.log(num2);
-//console.log(arr3);
+    // テキスト取得
+    const text = backButton.parentNode.firstElementChild.innerText;
+    createIncompleteList(text);
+  });
 
-// 配列のコピー、結合
-//const arr4 = [10, 20];
-//const arr5 = [30, 40];
-//
-//const arr6 = [...arr4];
-//arr6[0] = 100;
-//console.log(arr6);
-//console.log(arr4);
+  // divタグの子要素に各要素を設定
+  addTarget.appendChild(li);
+  addTarget.appendChild(backButton);
 
-//const arr7 = [...arr4, ...arr5];
-//console.log(arr7);
+  // 完了リストに追加
+  document.getElementById("complete-list").appendChild(addTarget);
+  });
 
-//const arr8 = arr4;
-//arr8[0] = 100;
-//console.log(arr4);
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
 
-/**
-* mapやfilterを使った配列の処理
-*/
-//const nameArr = ["田中", "山田", "新屋"];
-// 従来のforループ
-//for(let index = 0; index < nameArr.length; index++) {
-//    console.log(`${index + 1}番目は${nameArr[index]}です。`);
-//}
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
-//const nameArr2 = nameArr.map((name)=>{
-//    return name;
-//});
-//console.log(nameArr2);
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+}
 
-// mapを使った処理
-//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
-
-//const numArr = [1, 2, 3, 4, 5];
-//const newNumArr = numArr.filter((num) => {
-//    return num % 2 === 1;
-//});
-//console.log(newNumArr);
-
-//const newNameArr = nameArr.map((name) => {
-//    if (name === "新屋") {
-//        return name;
-//    } else {
-//        return `${name}さん`;
-//    }
-//})
-//console.log(newNameArr);
-
-/**
-* 三項演算子
-*/
-// ある条件 ? 条件がtrueの時 : 条件がfalseの時
-//const val1 = 1 < 0 ? 'trueです。' : 'falseです。';
-//console.log(val1);
-//
-//const num = 1300;
-//
-//const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
-//console.log(formattedNum);
-
-//const checkSum = (num1, num2) => {
-//return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です。';
-//}
-//console.log(checkSum(50, 40));
-
-/**
-* 論理演算子の本当の意味を知ろう && ||
-*/
-//const flag1 = true;
-//const flag2 = true;
-//
-//if(flag1 || flag2){
-//    console.log("1か2はtrueになります。");
-//}
-//if(flag1 && flag2){
-//    console.log("1も2もtrueになります。");
-//}
-
-// || は左側がfalseなら右側を返す。
-//const num = 100;
-//const fee = num || "金額未設定です。";
-//console.log(fee);
-
-// && は左側がtrueなら右側を返す。
-//const num2 = null;
-//const fee2 = num2 && "なにか設定されました。";
-//console.log(fee2);
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
 
